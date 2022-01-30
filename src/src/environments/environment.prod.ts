560e1406e6e1d971547b6f96ea2847b52edfc374
Дима Киреев <dimanoit@gmail.com>
2022-01-30 18:49:47 +0200

    add env

--- src/src/environments/environment.prod.ts
+++ src/src/environments/environment.prod.ts
@@ -1,5 +1,9 @@
 import { AuthSettings } from 'src/app/auth/models/auth-settings';
 
+export enum ServerUrls {
+  Identity = 'https://localhost:5002',
+}
+
 export const environment = {
   production: true,
   identitySettings: {} as AuthSettings,
